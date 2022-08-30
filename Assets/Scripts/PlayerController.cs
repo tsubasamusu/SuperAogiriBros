d763780b359712359ccb92432d75e467edd9c1b7
tsubasamusu <rdr2daisuki@gmail.com>
2022-08-30 14:31:55 +0900

    「PlayerController.cs」に左右移動の処理を追加した。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float moveSpeed;//移動速度（仮）
+
+    [SerializeField]
+    private Rigidbody rb;//RigidBody
+
+    private float moveDirection;//移動方向
+
+    private Vector3 movement;//移動位置
+
+    /// <summary>
+    /// 毎フレーム呼び出される
+    /// </summary>
+    private void Update()
     {
-        
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 一定時間ごとに呼び出される
+    /// </summary>
+    private void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + movement);
+    }
+
+    /// <summary>
+    /// プレーヤーの行動を制御する
+    /// </summary>
+    private void Move()
+    {
+        //移動方向を取得
+        moveDirection = Input.GetAxis("Horizontal");
+
+        //TODO:GameDataから移動速度を取得する処理
+
+        //移動位置を設定
+        movement = new Vector3(-1f,transform.position.y,0f) * moveDirection * moveSpeed * Time.fixedDeltaTime;
+
+        //ジャンプキーが押されたら
+        if(Input.GetAxis("Vertical")>0)
+        {
+
+        }
     }
 }
